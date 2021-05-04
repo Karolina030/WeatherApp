a37c911effcc971ace6b7688afd60cbc3dcb20c8
Karolina030 <60924470+Karolina030@users.noreply.github.com>
2021-05-04 15:07:15 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Karolina Matuszczyk on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
